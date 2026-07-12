3402fd577c98039de34009b6d44cc13c9199b352
itahmedosama-dotcom <it.ahmedosama@gmail.com>
2026-07-12 13:11:26 +0300

    Delete ADD_CROWD_SOUND.js

--- ADD_CROWD_SOUND.js
+++
@@ -1,9 +0,0 @@
-// Crowd sound integration
-const crowdAudio = new Audio('./assets/crowd-cheer.mp3');
-
-function playLoginSound(){
-  crowdAudio.currentTime = 0;
-  crowdAudio.play().catch(()=>{});
-}
-
-// Call playLoginSound() immediately after successful login.
